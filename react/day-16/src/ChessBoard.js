23d44419e42976ff9dc09e6a97aafe36d18c9485
Ayan Dhara <37933140+Ayan-Dhara@users.noreply.github.com>
2021-07-12 19:07:01 +0530

    day 16 2nd part

--- react/day-16/src/ChessBoard.js
+++ react/day-16/src/ChessBoard.js
@@ -3,18 +3,20 @@ import './ChessBoard.css';
 import ChessBoardRow from "./ChessBoardRow";
 
 function ChessBoard() {
-    return (
-        <div className="chess-board">
-            <ChessBoardRow/>
-            <ChessBoardRow/>
-            <ChessBoardRow/>
-            <ChessBoardRow/>
-            <ChessBoardRow/>
-            <ChessBoardRow/>
-            <ChessBoardRow/>
-            <ChessBoardRow/>
-        </div>
-    );
+  return (
+    <div className="chess-board-div">
+      <div className="chess-board">
+        <ChessBoardRow/>
+        <ChessBoardRow/>
+        <ChessBoardRow/>
+        <ChessBoardRow/>
+        <ChessBoardRow/>
+        <ChessBoardRow/>
+        <ChessBoardRow/>
+        <ChessBoardRow/>
+      </div>
+    </div>
+  );
 }
 
 export default ChessBoard;
