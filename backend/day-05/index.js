c3210208bdbca56d771e3a61c07c307839e4eb47
Ayan Dhara <37933140+Ayan-Dhara@users.noreply.github.com>
2021-09-03 22:42:11 +0530

    Update index.js

--- backend/day-05/index.js
+++ backend/day-05/index.js
@@ -1,34 +1,109 @@
-const fs = require('fs')
-const args = process.argv
+#!node
 
-args.shift()
-args.shift()
+const fs = require('fs');
 
-let option = null;
+(()=> {
+    const args = process.argv
 
-let data = null;
-let func = null;
-let type = null;
-let name = null;
+    let option = null;
+    let help;
+    let action;
+    let type;
+    let path;
+    let data;
+    let to;
+    let encoding = 'utf-8';
 
-while(option = args.shift()){
-    let value = args.shift()
-    switch (option){
-        case "--action":
-            func = value
+    while (option = args.shift())
+        if (option === __filename)
             break
-        case "--data":
-            data = value
-            break
-        case "--type":
-            type = value
-            break
-        case "--name":
-            name = value
+
+    if (args.length === 0)
+        help = true
+
+    while (option = args.shift()) {
+        switch (option) {
+            case "--help":
+            case "-h":
+                help = true
+                break
+            case "--action":
+            case "-a":
+                action = args.shift()
+                break
+            case "--data":
+            case "-d":
+                data = args.shift()
+                break
+            case "--type":
+            case "-t":
+                type = args.shift()
+                break
+            case "--path":
+            case "-p":
+                path = args.shift()
+                break
+            case "--to":
+            case "-o":
+                to = args.shift()
+                break
+            default:
+                console.log("Unknown Argument:", option)
+        }
     }
-}
 
-switch (func){
-    case "create":
-        create(type, )
-}
+    if (help)
+        console.log(`   HELP:
+    --action   -a [ create / rename / delete (remove) / append / rewrite (update) / read (show) ]
+    --type     -t [ file / folder ]
+    --path     -p [ name of file or folder ]
+    --to       -o [ new name of file or folder ]
+    --data     -d [ data to append or write ]
+    --encoding -e [ encoding of file to read or write (default utf-8) ]`)
+    else if (path === 'index.js' || path === '.gitignore')
+        console.log("Cannot do anything with index.js")
+    else
+        switch (action) {
+            case "create":
+                switch (type) {
+                    case "file":
+                        fs.writeFileSync(path, data, encoding)
+                        break
+                    case "folder":
+                        fs.mkdirSync(path)
+                        break
+                    default:
+                        console.log("Unknown type:", type)
+                }
+                break
+            case "delete":
+            case "remove":
+                switch (type) {
+                    case "file":
+                        fs.unlinkSync(path)
+                        break
+                    case "folder":
+                        fs.rmdirSync(path)
+                        break
+                    default:
+                        console.log("Unknown type:", type)
+                }
+                break
+            case "rename":
+                fs.renameSync(path, to)
+                break
+            case "read":
+            case "view":
+                console.log(fs.readFileSync(path, encoding))
+                break
+            case "append":
+                fs.appendFileSync(path, data, encoding)
+                break
+            case "rewrite":
+            case "update":
+                fs.writeFileSync(path, data, to)
+                break
+            default:
+                console.log("Unknown Action:", action)
+        }
+})()
